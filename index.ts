3d584826491d98d8198129c1ae292718c2756d0b
JadMashtoub3 <103596586@student.swin.edu.au>
2021-08-15 23:25:08 +1000

    2 files modified, 3 added, 2 deleted

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
-
+import { Colours } from './models/colours.enum';
+import { Dice } from './models/dice.enum';
+import { Player } from './models/player';
 // #region tic tac toe
 
 // NOTE: 'TTT' stands for Tic Tac Toe
@@ -170,3 +172,194 @@ function aiMove() {
 init();
 
 // #endregion
+let p1: Player = new Player('Player 1');
+let p2: Player = new Player('Player 2');
+
+let players: Player[] = [p1, p2];
+let currentplayerid = 0;
+
+let diceSides = 0;
+let numDice = 0;
+
+let scoreToWin = 0;
+
+const setupDiv: HTMLElement = document.getElementById('setup');
+const playersDiv: HTMLElement = document.getElementById('players');
+const turnDiv: HTMLElement = document.getElementById('turn');
+const diceDiv: HTMLElement = document.getElementById('dice');
+
+const p1nameInput: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById('player1-name-inp')
+);
+const p2nameInput: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById('player2-name-inp')
+);
+const p1colour: HTMLSelectElement = <HTMLSelectElement>(
+  document.getElementById('player1-colour')
+);
+const p2colour: HTMLSelectElement = <HTMLSelectElement>(
+  document.getElementById('player2-colour')
+);
+
+const diceSelect: HTMLSelectElement = <HTMLSelectElement>(
+  document.getElementById('dice-slt')
+);
+const diceNoInput: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById('dice-no-inp')
+);
+
+// score to win input field
+const scoreNoInput: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById('score-no-inp')
+);
+
+// start game button, with onclick behaviour
+const startButton: HTMLElement = document.getElementById('start-btn');
+startButton.addEventListener('click', startGame);
+
+// player names
+const p1name: HTMLElement = document.getElementById('player1-name');
+const p2name: HTMLElement = document.getElementById('player2-name');
+
+// player scores
+const p1score: HTMLElement = document.getElementById('player1-score');
+const p2score: HTMLElement = document.getElementById('player2-score');
+
+// element for displaying name of play whose turn it is
+const turnDisplay: HTMLElement = document.getElementById('turn-player');
+
+// roll button, with onclick behaviour
+const rollButton: HTMLElement = document.getElementById('roll-btn');
+rollButton.addEventListener('click', roll);
+
+// display the winner of the previous game
+const winner: HTMLElement = document.getElementById('winner');
+
+/**
+ * add options to the dice dropdown and player colour dropdowns based on dice and colour enums
+ */
+function enumInit() {
+  for (let d in Dice) {
+    if (isNaN(Number(d))) {
+      let newOption: HTMLOptionElement = document.createElement('option');
+      newOption.innerHTML = d;
+      newOption.value = Dice[d].toString();
+      diceSelect.add(newOption);
+    }
+  }
+  // player 1 colour dropdown
+  let count = 0;
+  for (let c in Colours) {
+    if (isNaN(Number(c))) {
+      let newOption: HTMLOptionElement = document.createElement('option');
+      newOption.innerHTML = c;
+      newOption.value = count.toString();
+      count++;
+      p1colour.add(newOption);
+    }
+  }
+  // player 2 colour dropdown
+  count = 0;
+  for (let c in Colours) {
+    if (isNaN(Number(c))) {
+      let newOption: HTMLOptionElement = document.createElement('option');
+      newOption.innerHTML = c;
+      newOption.value = count.toString();
+      count++;
+      p2colour.add(newOption);
+    }
+  }
+  // set player 2 to the second option so by default players have different colours
+  p2colour.value = '1';
+}
+enumInit();
+
+/**
+ * called by the start game button, hides setup, assigns setup input values to variables used and displays game elements
+ */
+function startGame() {
+  // hide setup
+  setupDiv.setAttribute('style', `display: none`);
+
+  // set player names
+  p1.name = p1nameInput.value;
+  p2.name = p2nameInput.value;
+
+  // display set player names
+  p1name.innerHTML = p1.name;
+  p2name.innerHTML = p2.name;
+
+  // setup dice and win value
+  diceSides = Number(diceSelect.value);
+  numDice = Number(diceNoInput.value);
+  scoreToWin = Number(scoreNoInput.value);
+
+  // display game
+  playersDiv.setAttribute('style', `display:''`);
+  turnDiv.setAttribute('style', `display:''`);
+  diceDiv.setAttribute('style', `display:''`);
+}
+
+function roll() {
+  // set player colours to the values in the dropdowns (since dropdowns are not fixed per game)
+  p1.colour = Colours[p1colour.value];
+  p2.colour = Colours[p2colour.value];
+
+  // select the current player
+  let currentplayer = players[currentplayerid];
+
+  // clear previous roll
+  diceDiv.innerHTML = '';
+
+  // update owner/roller of dice
+  let rolledDisplay = document.createElement('div');
+  rolledDisplay.innerHTML = `Dice rolled by ${currentplayer.name}`;
+  diceDiv.appendChild(rolledDisplay);
+
+  for (let n = 0; n < numDice; n++) {
+   
+    const dice = Math.floor(Math.random() * diceSides) + 1;
+
+ 
+    let dieDiv: HTMLElement = document.createElement('div');
+    dieDiv.innerHTML = String(dice);
+    dieDiv.classList.add('die');
+    dieDiv.setAttribute('style', `background-color: ${currentplayer.colour}`);
+    diceDiv.appendChild(dieDiv);
+
+
+    currentplayer.rolls.push(dice);
+    currentplayer.score += dice;
+  }
+
+  
+  if (currentplayer.score >= scoreToWin) {
+    window.alert(`Winner is ${currentplayer.name}`);
+  
+    winner.innerHTML = `Winner of last game was ${
+      currentplayer.name
+    } with a score of ${currentplayer.score}`;
+   
+    for (let p = 0; p < players.length; p++) {
+      players[p].score = 0;
+      players[p].rolls = [];
+    }
+   
+    currentplayerid = 0;
+
+    setupDiv.setAttribute('style', `display:''`);
+
+  
+    playersDiv.setAttribute('style', `display:none`);
+    turnDiv.setAttribute('style', `display:none`);
+    diceDiv.setAttribute('style', `display:none`);
+
+    diceDiv.innerHTML = '';
+  } else {
+  
+    if (currentplayerid === players.length - 1) {
+      currentplayerid = 0;
+   
+    }
+  }
+}
